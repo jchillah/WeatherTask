6579df883f8ab66d7bebd2bd2eee4e46d488d7dd
Jchillah <jchillah@gmail.com>
2025-02-15 12:59:47 +0100

    Initial commit

---
+++ WeatherTask/WeatherTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTaskApp.swift
+//  WeatherTask
+//
+//  Created by Michael Winkler on 15.02.25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
